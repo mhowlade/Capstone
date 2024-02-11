764e5b8bc349868725499657e85ef4350afcbdd3
Alice Smith <alice@smith.com>
2024-02-11 10:15:33 -0500

    recommended purchase on cart

--- client/src/cart.jsx
+++ client/src/cart.jsx
@@ -2,18 +2,33 @@ import { useState, useEffect } from "react";
 
 function Cart() {
   const [checkout, setCheckout] = useState(false);
-  const [purchase, setPurchase] = useState()
+  const [purchase, setPurchase] = useState();
+ 
+  
+  useEffect(() => {
+    LoadRecommendations()
+    console.log('cart page useeffect', purchase)
+  }, [])
 
-
-
-  let cartItems = []
-  let existing = sessionStorage.getItem("cart");
-  if (existing != null)
-  {
-    cartItems = JSON.parse(existing)
-  }
+    let cartItems = []
+    let existing = sessionStorage.getItem("cart");
+    if (existing != null)
+    {
+      cartItems = JSON.parse(existing)
+    }
+  
 
   console.log('cart items', cartItems)
+  console.log('cart item id', cartItems[0]._id)
+
+  function LoadRecommendations(){
+    fetch(
+      "http://localhost:3000/api/recommendations/" + cartItems[0]._id + "/4")
+      .then((res) => res.json())
+      .then((data) => setPurchase(data))
+  };
+
+
   
   return (
     <>
@@ -26,6 +41,15 @@ function Cart() {
               <li>{el.name}</li>
               <li>{el.price}</li>
             </div>
+            </>
+          ))}
+        </ul>
+        <h2>Based on your purchase you might also like:</h2>
+        <ul>
+          {purchase?.map((el,index) => (
+            <>
+            <img src={el.images}></img>
+            <p>{el.name}</p>
             </>
           ))}
         </ul>
--- client/src/home.jsx
+++ client/src/home.jsx
@@ -1,12 +1,15 @@
 import "./styles/home.css"
+import { useEffect, useState } from "react";
 
 function Home(props) {
-  
+   
   return (
     <>
     <div className='homepage'>
       <section id="featured">
         <h2>Today's Featured Friends</h2>
+
+        <h2>All of our Friends</h2>
         <ul>
         {props.categories?.map((el, index) => (           
             <><a href={"/" + el.name}>{el.name}</a><p>{el.image_url}</p></>
