2d69eacc20d868692a26b2759955dd315406abe5
Alice Smith <alice@smith.com>
2024-02-11 22:05:23 -0500

    Good to go

--- client/src/home.jsx
+++ client/src/home.jsx
@@ -1,20 +1,40 @@
 import "./styles/home.css"
+import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 function Home(props) {
-   
+  let [recItems, setRecItems] = useState([]);
+  useEffect(()=>{LoadRecs()},[]);
+
+  function LoadRecs() {
+    fetch("http://localhost:3000/api/popular/5")
+      .then((res) => res.json())
+      .then((data) =>setRecItems(data) );  }
+
+  function setSelected(value){
+    sessionStorage.setItem('selected',JSON.stringify(value))
+  }
   return (
     <>
     <div className='homepage'>
       <section id="featured">
-        <h2>Today's Featured Friends</h2>
-
-        <h2>All of our Friends</h2>
+        <h1>Featured pets!</h1>
+      <div className="cart-recs">
         <ul>
-        {props.categories?.map((el, index) => (           
-            <><a href={"/" + el.name}>{el.name}</a><p>{el.image_url}</p></>
-        ))}
+          {recItems?.map((el, index) => (
+            <>
+            <div className="product-container" onClick={()=>{setSelected(el)}}>
+            <Link to="/details" style={{ textDecoration: "none", color: "black" }}>
+              <li>{el.name}</li>
+              <li>Age: {el.age}</li>
+              <li>${el.price}</li>
+              <li><img src={el.images} /></li>
+              </Link>
+            </div>
+            </>
+          ))}
         </ul>
+      </div>
       </section>
     </div>
     </>
