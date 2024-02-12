73a244a358661abdf923ed96b06f7bec153bdfc9
Alice Smith <alice@smith.com>
2024-02-12 13:45:40 -0500

    search function

--- client/src/searchfunction.jsx
+++ client/src/searchfunction.jsx
@@ -5,6 +5,7 @@ function SearchFunction(){
     const [products, setProducts] = useState([]);
     const [categories, setCategories] = useState([]);
     const [searchResults, setSearchResults] = useState([]);
+    let [info, setInfo] = useState([]);
     
     useEffect(() => {
         GetInfo()
@@ -16,14 +17,14 @@ function SearchFunction(){
         setProducts(JSON.parse(productsdata))
 
     }
-    
-}
-    
     let datatype = sessionStorage.getItem("display_type")
     if (datatype != null && datatype !=''){
-    const info = products.filter((products) =>
+    const infoD = products.filter((products) =>
       products.animal.includes(datatype)
-    );} 
+    );
+    setInfo(infoD);} 
+    
+    }
 
     function handleSubmit(event){
         event.preventDefault();
@@ -33,26 +34,41 @@ function SearchFunction(){
 
 
     async function searchProduct(){
-        const results =await datatype.filter((searchTerm) =>{
+        const results = info.filter((product) =>{
             if(
-                products.name.includes(searchTerm) ||
-                products.age.toString().includes(searchTerm) ||
-                products.price.toString().includes(searchTerm)){
+                product.name.includes(searchTerm) ||
+                product.age.toString().includes(searchTerm) ||
+                product.price.toString().includes(searchTerm)){
                     return true
                 }
-            }); setSearchResults(results); 
+            }); console.log(results);setSearchResults(results); 
         }
 
     return(
         <>
+        <div className="search-wrap">
         <form className="search" onSubmit={handleSubmit}>
             <label></label>
             <input type='text' value={searchTerm} placeholder= 'Search' onChange={(event) => setSearchTerm(event.target.value)}/>
-            <button>Search</button>
+            <button onClick={(e)=>(searchProduct())}>Search</button>
         </form>
-        <div>
         
-    
+        <div className="search-products">
+        <div className="cart-products">
+        <ul>
+          {searchResults?.map((el, index) => (
+            <>
+            <div className="product-container">
+              <li>{el.name}</li>
+              <li>Age: {el.age}</li>
+              <li>${el.price}</li>
+              <li><img src={el.images} /></li>
+            </div>
+            </>
+          ))}
+        </ul>
+      </div>
+        </div>
         </div>
         </>
 
